f4e4f19a8f450daae2d6a8f52ff3c847937924ef
Stephen <st-jeeves@hotmail.co.uk>
2020-02-24 15:33:39 +0000

    getters and setters added

--- class_homework.rb
+++ class_homework.rb
@@ -7,21 +7,33 @@ class Festival
     @acts = acts
   end
 
-def get_name()
-  return @name
-end
+  def get_name()
+    return @name
+  end
 
-def get_location()
-  return @location
-end
+  def get_location()
+    return @location
+  end
 
-def get_ticket_price
-  return @ticket_price
-end
+  def get_ticket_price()
+    return @ticket_price
+  end
 
-def get_location
-  return @location
-end
+  def get_acts()
+    return @acts
+  end
+
+  def set_name(new_name)
+    @name = new_name
+  end
+
+  def set_location(new_location)
+    @location = new_location
+  end
+
+  def set_ticket_price(new_ticket_price)
+    @ticket_price = new_ticket_price
+  end
 
 
 
--- specs/class_homework_spec.rb
+++ specs/class_homework_spec.rb
@@ -15,6 +15,32 @@ class TestFestival < MiniTest::Test
     assert_equal("Sziget", @festival.get_name())
   end
 
-  def test_get
+  def test_get_location()
+    assert_equal("Budapest", @festival.get_location())
+  end
+
+  def test_get_ticket_price()
+    assert_equal(250, @festival.get_ticket_price())
+  end
+
+  def test_get_acts()
+    assert_equal(["Calvin Harris",
+      "Caribou", "Lewis Capaldi" ], @festival.get_acts())
+  end
+
+  def test_set_name()
+    @festival.set_name("Steve-Fest")
+    assert_equal("Steve-Fest", @festival.get_name())
+  end
+
+  def test_set_location()
+    @festival.set_location("Edinburgh")
+    assert_equal("Edinburgh", @festival.get_location())
+  end
+
+  def test_set_ticket_price()
+    @festival.set_ticket_price(200)
+    assert_equal(200, @festival.get_ticket_price())
+  end
 
 end
